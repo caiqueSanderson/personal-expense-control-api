9dc8e6ec06f107f72314b0ef30ba380499aafa55
sanderson <caique.borges@al.infnet.edu.br>
2025-08-18 20:57:17 -0300

    Initial commit

---
+++ src/main/java/com/infnet/personal_expense_control_api/PersonalExpenseControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.personal_expense_control_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalExpenseControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalExpenseControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infnet/personal_expense_control_api/PersonalExpenseControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.personal_expense_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExpenseControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
